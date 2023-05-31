d4945a3167daadbac9cfbecd8f0be699933b94a1
Katrin <meowmanyak@gmail.com>
2023-05-31 22:13:35 +0200

    Changed text about me and photo

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import photo from "../images/me.jpg";
+import photo from "../images/photo.jpg";
 import Footer from "../Footer";
 
 class About extends React.Component {
@@ -7,25 +7,59 @@ class About extends React.Component {
     return (
       <div className="About">
         <div className="content-container container">
-          <div className="row mb-5">
-            <div className="col-lg-6 col-sm-12">
-              <h1>Hi, I am Katrin Pavlova</h1>
-              <h2>Frontend developer based in the Netherlands.</h2>
+          <h2 className="text-center">About me</h2>
+          <p className="text-center ">
+            My name is Katrin, I am from Ukraine, currently based in the
+            Netherlands. I'm looking for a new start in career.
+          </p>
+          <div className="row mb-5 mt-5">
+            <div className="col-lg-7 col-sm-12">
               <p>
-                I like to learn something new and face new challenges. This
-                workshop was a nice chance to refresh my previous knowledges and
-                learn something new. Lessons have good structure and gives a lot
-                of coding practice.
+                I hold a Master's degree in Software Engineering, which has
+                provided me with a comprehensive understanding of the
+                theoretical and practical aspects of this dynamic field.
+                Throughout my educational journey, I delved into various
+                subjects related to software development, including object
+                oriented design, software architecture, algorithms, data
+                structures, and software testing. The program equipped me with a
+                solid foundation in programming languages, such as Java, C#,
+                Python and C++, and honed my skills in software design and
+                development methodologies.
               </p>
-              <a
-                href="/work"
-                className="btn btn-branding"
-                title="Work of Katrin Pavlova"
-              >
-                See my projects
-              </a>
+              <p>
+                Recently I refreshed my knowledge by completing courses by
+                SheCodes, I have acquired a comprehensive understanding of
+                front-end technologies. Proficient in HTML, CSS, and JavaScript,
+                I possess the ability to transform designs into pixel-perfect,
+                responsive web pages. Additionally, I have experience working
+                with React framework, enabling me to build dynamic and
+                interactive web applications.
+              </p>
+              <p>
+                What truly fuels my passion is the opportunity to bring ideas to
+                life through coding. I love the creative process of translating
+                concepts into functional software that leave a lasting impact.
+              </p>
+              <p>
+                Outside of development, I have a lot of interests like playing
+                guitar, making paper models, mountain hiking, traveling. Also I
+                am and I found an opportunity to make this hobby work. I spent
+                some time making money from live streaming and later I become an
+                account seller. I was a leader of a small team and we provided
+                different services to other gamers. I enjoyed this period of
+                time spent in fun and profitable way of playing games. I think
+                the ideal job is one that brings pleasure and money at the same
+                time.
+              </p>
+              <p>
+                Thank you for visiting my portfolio! I am excited to connect
+                with like-minded individuals and organizations that share the
+                same enthusiasm for pushing boundaries and crafting remarkable
+                online experiences. Let's collaborate and bring our ideas to
+                life!
+              </p>
             </div>
-            <div className="col-lg-6 col-sm-12">
+            <div className="col-lg-5 col-sm-12">
               <img
                 src={photo}
                 alt="Photo of Katrin Pavlova"
